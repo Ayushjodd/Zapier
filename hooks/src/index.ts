78ee27190d69cc19faf93bb4a393459457b7a176
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-14 23:26:05 +0530

    finished 33.1

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,8 +1,29 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+
+  await client.$transaction(async (tx) => {
+    const run = await client.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: {},
+      },
+    });
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+
+    res.json({
+      message: "created",
+    });
+  });
 });
